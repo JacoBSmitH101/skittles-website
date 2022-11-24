835d88a79dbe46aee74b3549c033dc6944d6ad84
Jacob Smith <jacobsmith2005@hotmail.com>
2022-11-24 21:49:56 +0000

    account popover should work

--- src/components/account-popover.js
+++ src/components/account-popover.js
@@ -66,7 +66,46 @@ export const AccountPopover = (props) => {
     });
   }, []);
   if (!authenticated) {
-    return <p>not authenticated</p>;
+    return (<Popover
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        horizontal: "left",
+        vertical: "bottom",
+      }}
+      onClose={onClose}
+      open={open}
+      PaperProps={{
+        sx: { width: "300px" },
+      }}
+      {...other}
+    >
+      <Box
+        sx={{
+          py: 1.5,
+          px: 2,
+        }}
+      >
+        <Typography variant="overline">NOT AUTHORISED</Typography>
+        <Typography color="text.secondary" variant="body2">
+          not authorised user
+        </Typography>
+      </Box>
+      <MenuList
+        disablePadding
+        sx={{
+          "& > *": {
+            "&:first-of-type": {
+              borderTopColor: "divider",
+              borderTopStyle: "solid",
+              borderTopWidth: "1px",
+            },
+            padding: "12px 16px",
+          },
+        }}
+      >
+        <MenuItem onClick={handleSignOut}>Sign out</MenuItem>
+      </MenuList>
+    </Popover>);
   }
   return (
     <Popover
