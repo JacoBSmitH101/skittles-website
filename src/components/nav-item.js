92a38ceba04f8e0c28d3c1a9529d9d85ae652999
JacoBSmitH101 <47941474+JacoBSmitH101@users.noreply.github.com>
2022-11-30 18:30:32 +0000

    Sidebar updaes

--- src/components/nav-item.js
+++ src/components/nav-item.js
@@ -1,13 +1,14 @@
 import NextLink from 'next/link';
 import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
-import { Box, Button, ListItem } from '@mui/material';
+import { Box, Button, createTheme, ListItem, withStyles } from '@mui/material';
 
 export const NavItem = (props) => {
   const { href, icon, title, enabled, ...others } = props;
   const router = useRouter();
   const active = href ? (router.pathname === href) : false;
-
+  //set text colour to secondary.main if active, neutral.400 if not active and if not enabled set to neutral.300
+  const textColor = enabled ? (active ? 'secondary.main' : 'neutral.400') : 'secondary.light';
   return (
     <ListItem
       disableGutters
@@ -29,9 +30,9 @@ export const NavItem = (props) => {
           disableRipple
           disabled={!enabled} 
           sx={{
-            backgroundColor: active && 'rgba(255,255,255, 0.08)',
+            backgroundColor: enabled ? active && 'rgba(255,255,255, 0.08)' : active && 'rgba(255,255,255, 0.04)',
             borderRadius: 1,
-            color: active ? 'secondary.main' : 'neutral.300',
+            color: textColor,
             fontWeight: active && 'fontWeightBold',
             justifyContent: 'flex-start',
             px: 3,
@@ -39,15 +40,15 @@ export const NavItem = (props) => {
             textTransform: 'none',
             width: '100%',
             '& .MuiButton-startIcon': {
-              color: active ? 'secondary.main' : 'neutral.400'
+              color: active ? 'secondary.main' : (enabled ? 'neutral.400' : 'rgba(255,255,255, 0.24)'),
             },
             '&:hover': {
               backgroundColor: 'rgba(255,255,255, 0.08)'
-            }
+            },
           }}
         >
           <Box sx={{ flexGrow: 1 }}>
-            {title}
+            {title} 
           </Box>
         </Button>
       </NextLink>
