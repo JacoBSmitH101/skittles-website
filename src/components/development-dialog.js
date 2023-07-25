bd34556cfa0373ac3549b375d58b21b57400bbe2
JacoBSmitH101 <47941474+JacoBSmitH101@users.noreply.github.com>
2023-07-25 20:08:09 +0100

    added do not show again options to development dialog

--- src/components/development-dialog.js
+++ src/components/development-dialog.js
@@ -1,26 +1,48 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material'
-import React, { useState } from 'react'
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Checkbox, FormControlLabel } from '@mui/material'
+import React, { useEffect, useState } from 'react'
 
 function DevelopmentDialog({ page }) {
-    const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(true);
+  const [dontShowAgain, setDontShowAgain] = useState(false);
+
+  useEffect(() => {
+    const dontShow = localStorage.getItem(`dontShow${page}`);
+    if (dontShow === 'true') {
+      setOpen(false);
+    }
+  }, [page]);
+
   const handleClose = () => {
+    if (dontShowAgain) {
+      localStorage.setItem(`dontShow${page}`, 'true');
+    }
     setOpen(false);
   };
+
+  const handleCheckboxChange = (event) => {
+    setDontShowAgain(event.target.checked);
+  };
+
   return (
     <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>{"Development Notice"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            The {page} page is currently in development. Some features may not work as expected.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary" autoFocus>
-            Continue
-          </Button>
-        </DialogActions>
-      </Dialog>
+      <DialogTitle>{"Development Notice"}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          The {page} page is currently in development. Some features may not work as expected.
+        </DialogContentText>
+        
+      </DialogContent>
+      <DialogActions>
+      <FormControlLabel
+          control={<Checkbox checked={dontShowAgain} onChange={handleCheckboxChange} />}
+          label="Don't show again"
+        />
+        <Button onClick={handleClose} color="primary" autoFocus>
+          Continue
+        </Button>
+      </DialogActions>
+    </Dialog>
   )
 }
 
-export default DevelopmentDialog
+export default DevelopmentDialog;
